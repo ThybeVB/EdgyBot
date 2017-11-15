d13f04cc3b3ee936b4f6eec2447a6b8e180ffd81
MonstahGames <30873388+MonstahGames@users.noreply.github.com>
2017-11-15 16:28:22 +0100

    Fixed event sutff

--- EdgyBot/EdgyBot/Program.cs
+++ EdgyBot/EdgyBot/Program.cs
@@ -24,7 +24,6 @@ namespace EdgyBot
             Console.ForegroundColor = ConsoleColor.Green;
             _client.Log += Log;
             _client.Ready += Ready;
-            _client.UserJoined += UserJoined;
             _client.UserLeft += UserLeft;
             string _token = "";
             await _client.LoginAsync(TokenType.Bot, _token);
@@ -43,17 +42,9 @@ namespace EdgyBot
             Console.WriteLine(message.ToString());
             return Task.CompletedTask;
         }
-        private async Task UserJoined(SocketGuildUser user)
-        {
-            IDMChannel dm = await Context.User.GetOrCreateDMChannelAsync();
-
-            string guildname = Context.Guild.Name;
-            await Context.Channel.SendMessageAsync("Welcome to " + guildname + ", " + Context.User.Username + "!");
-            await dm.SendMessageAsync("Welcome to " + guildname + ", " + Context.User.Username + "!");
-        }
         private async Task UserLeft(SocketGuildUser user)
         {
-            await Context.Channel.SendMessageAsync("Sad to see you leave, " + Context.User.Username);
+            await ReplyAsync("Sad to see you leave, " + user.Mention + "!");
         }
     }
 }
