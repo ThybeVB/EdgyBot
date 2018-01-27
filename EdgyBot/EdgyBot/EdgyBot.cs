339dbe9ee40521a9a133f7c509d98c4f6d21761b
MonstahGames <monstahhhgames@gmail.com>
2018-01-27 09:52:44 +0100

    Improved EdgyBot.cs a little

--- EdgyBot/EdgyBot/EdgyBot.cs
+++ EdgyBot/EdgyBot/EdgyBot.cs
@@ -23,15 +23,14 @@ namespace EdgyBot
         private static void Main(string[] args)
             => new EdgyBot().StartAsync().GetAwaiter().GetResult();
 
-        public readonly DiscordSocketClient Client = new DiscordSocketClient(new DiscordSocketConfig{LogLevel = LogSeverity.Verbose, DefaultRetryMode = RetryMode.RetryRatelimit});
+        public readonly DiscordSocketClient Client = new DiscordSocketClient(new DiscordSocketConfig{LogLevel = LogSeverity.Verbose});
         private readonly CommandHandler  _handler = new CommandHandler();
         private readonly LibEdgyBot _lib = new LibEdgyBot();
-        private EventHandler _ehandler;
 
         private async Task StartAsync ()
         {
             Console.ForegroundColor = ConsoleColor.Green;
-            _ehandler = new EventHandler(Client);
+            new EventHandler(Client);
             await Client.LoginAsync(TokenType.Bot, _lib.GetToken());
             await Client.StartAsync();          
             await _handler.InitializeAsync(Client);
