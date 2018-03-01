827d18d6e186647754f145bad451f77a14fc7bc0
MonstahGames <monstahhhgames@gmail.com>
2018-03-01 19:54:58 +0100

    s

---
+++ EdgyBot/EdgyBot/GDLib.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdgyBot
+{
+    //TODO
+
+    public class GDLib
+    {
+        public string getUser21 ()
+        {
+            return null;
+        }
+    }
+}
