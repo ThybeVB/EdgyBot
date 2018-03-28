d9748d67c557783f8aa7ccdad75565cd784f7de8
MonstahGames <30873388+MonstahGames@users.noreply.github.com>
2018-03-28 15:51:45 +0200

    Removed Blacklist Commands. (Violation Discord ToS)

--- EdgyBot/EdgyBot/Modules/Categories/AdminCommands.cs
+++ EdgyBot/EdgyBot/Modules/Categories/AdminCommands.cs
@@ -44,54 +44,6 @@ namespace EdgyBot.Modules.Categories
                 await ReplyAsync("I'm not in that server! :cry:");
             }     
         }
-        [Command("announce", RunMode = RunMode.Async)]
-        public async Task AnnounceCmd([Remainder]string msg)
-        {
-            if (Context.User.Id != _lib.GetOwnerID())
-            {
-                await ReplyAsync("Permission Denied.");
-                return;
-            }
-            await _lib.EdgyLog(LogSeverity.Info, "Sending announcement " + msg);
-            int sentCount = 0;
-            foreach (IGuild guild in Context.Client.Guilds)
-            {
-                if (guild == null) continue;
-                if (!_database.IsServerBlacklisted(guild.Id))
-                {
-                    ITextChannel channel =  await guild.GetDefaultChannelAsync();
-                    if (channel == null) continue;
-                    
-                    Embed e = _lib.CreateAnnouncementEmbed(msg, true);
-                    try
-                    {
-                        await channel.SendMessageAsync("", embed: e);
-                        sentCount++;
-                    }
-                    catch
-                    {
-                        await _lib.EdgyLog(LogSeverity.Error, "Could not send announcement to " + guild.Name);
-                    }               
-                }
-            }
-            await ReplyAsync("Sent message to " + sentCount + " servers.");
-        }
-        [Command("stopannounce")][Name("stopannounce")][Summary("Stops receiving announcements from EdgyBot")]
-        [RequireUserPermission(GuildPermission.Administrator)]
-        public async Task StopAnnounceCmd()
-        {
-            _database.BlacklistServer(Context.Guild.Id);
-            Embed e = _lib.CreateEmbedWithText("Announcement Unsub", Context.Guild.Name + " has been excluded from recieving announcements.");
-
-            await ReplyAsync("", embed: e);
-        }
-        [Command("enableannounce")][Name("enableannounce")][Summary("Start recieving announcements again.")]
-        public async Task EnableAnnounceCmd ()
-        {
-            _database.DeleteFromBlacklisted(Context.Guild.Id);
-            Embed e = _lib.CreateEmbedWithText("Announcement Enable", Context.Guild.Name + " has started recieving announcements again.");
-            await ReplyAsync("", embed: e);
-        }
         [Command("setstatus")]
         public async Task SetStatusCmd([Remainder]string input)
         {
