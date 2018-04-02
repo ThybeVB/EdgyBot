5bdfa7fa5b671c42c2d153061128aa3be6fd986e
MonstahGames <30873388+MonstahGames@users.noreply.github.com>
2018-04-02 14:20:21 +0200

    Did what I can do for the first part

---
+++ netcore/EdgyBot/EdgyBot.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace EdgyBot
+{
+    public class EdgyBot
+    {
+        private static void Main()
+            => new EdgyBot().MainAsync().GetAwaiter().GetResult();
+
+        public readonly DiscordSocketClient Client = new DiscordSocketClient(new DiscordSocketConfig { LogLevel = LogSeverity.Verbose });
+        //private readonly LibEdgyBot _lib = new LibEdgyBot();
+
+        private async Task MainAsync ()
+        {
+            //new EventHandler(Client);
+            await Client.LoginAsync(TokenType.Bot, /*LoginInfo*/"");
+            await Client.StartAsync();
+            //await new CommandHandler().InitializeAsync(Client);
+            await Task.Delay(-1);
+        }
+    }
+}
