28810790fb998bd2091889d3e514126b6edf6934
MonstahGames <monstahhhgames@gmail.com>
2018-04-10 14:36:53 +0200

    Fix for Hosting

--- EdgyBot/EdgyBot/Handler/EventHandler.cs
+++ EdgyBot/EdgyBot/Handler/EventHandler.cs
@@ -26,10 +26,6 @@ namespace EdgyBot
             _client.Disconnected += Client_Disconnected;
         }
 
-        private async Task Client_Disconnected(Exception exception)
-            => await _lib.EdgyLog(LogSeverity.Critical, "EDGYBOT HAS SHUT DOWN WITH AN EXCEPTION, \n" + exception.Message);
-        
-
         public async Task Ready()
         {
             OwnerUser = _client.GetUser(_lib.GetOwnerID());
@@ -37,7 +33,22 @@ namespace EdgyBot
             string gameStatus = "e!help | EdgyBot for " + _client.Guilds.Count + " servers!";
             await _client.SetGameAsync(gameStatus);
             await _lib.EdgyLog(LogSeverity.Info, "Set game to " + gameStatus);
+
+            await UpdateHoster();
         }
+
+        //If the bot is idle for 30 minutes, the hosting service will shut it down. That's why this funcion exists
+        private async Task UpdateHoster ()
+        {
+            while (true)
+            {
+                await Task.Delay(TimeSpan.FromMinutes(25));
+                await _client.SetGameAsync("e!help | EdgyBot for " + _client.Guilds.Count + " servers!");
+            }
+        }
+
+        private async Task Client_Disconnected(Exception exception)
+            => await _lib.EdgyLog(LogSeverity.Critical, "EDGYBOT HAS SHUT DOWN WITH AN EXCEPTION, \n" + exception.Message);
 
         private static async Task Client_JoinedGuild(SocketGuild guild)
             => await guild.DefaultChannel.SendMessageAsync($"SH*T THANKS FOR INVITING ME M8'S, TO SEE ME COMMANDS, USE **{new LibEdgyBot().GetPrefix()}help**.");
