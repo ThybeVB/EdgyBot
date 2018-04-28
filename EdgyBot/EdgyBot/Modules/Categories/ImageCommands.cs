8aa40cf3a0b41bcf16dc6eff02d8eadcf3f8a828
MonstahGames <30873388+MonstahGames@users.noreply.github.com>
2018-04-28 20:05:12 +0200

    things

--- EdgyBot/EdgyBot/Modules/Categories/ImageCommands.cs
+++ EdgyBot/EdgyBot/Modules/Categories/ImageCommands.cs
@@ -1,20 +1,32 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using System.Linq;
 using ImageProcessor;
+using System.Net;
 
 namespace EdgyCore.Modules.Categories
 {
     public class ImageCommands : ModuleBase<SocketCommandContext>
     {
-        [Command("imgtest")][RequireOwner]
+        private readonly LibEdgyBot _lib = new LibEdgyBot();
+
+        [Command("imgtest", RunMode = RunMode.Async)][RequireOwner]
         public async Task ImgTest ()
         {
             Attachment img = Context.Message.Attachments.SingleOrDefault();
+
             if (img == null)
+            {
                 await ReplyAsync("Please provide an image.");
+                return;
+            }
+            try { _lib.DownloadImageAsync(img.Url); } 
+             catch {
+                await ReplyAsync("err");
+            }
         }
     }
 }
