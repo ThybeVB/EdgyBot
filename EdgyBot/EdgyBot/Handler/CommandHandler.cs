796f61a129523c35b04f2e13472798378c0f64e9
MonstahGames <monstahhhgames@gmail.com>
2018-05-04 20:07:18 +0200

    Improving the Command Handler

--- EdgyBot/EdgyBot/Handler/CommandHandler.cs
+++ EdgyBot/EdgyBot/Handler/CommandHandler.cs
@@ -9,6 +9,8 @@ namespace EdgyCore
 {
     public class CommandHandler
     {
+        private string _prefix;
+
         private DiscordSocketClient _client;
         private CommandService _service;
         private readonly LibEdgyBot _lib = new LibEdgyBot();
@@ -16,24 +18,28 @@ namespace EdgyCore
         public async Task InitializeAsync(DiscordSocketClient client)
         {
             _client = client;
-
+            _prefix = _lib.GetPrefix();
             _service = new CommandService();
+
             await _service.AddModulesAsync(Assembly.GetEntryAssembly());
             new HelpCommand(_service);
 
             _client.MessageReceived += HandleCommandAsync;
         }
+
         private async Task HandleCommandAsync(SocketMessage s)
         {
             SocketUserMessage msg = (SocketUserMessage)s;
             if (msg == null) return;
             SocketCommandContext context = new SocketCommandContext(_client, msg);
             int argPos = 0;
-            if (msg.HasStringPrefix(_lib.GetPrefix(), ref argPos))
+            if (msg.HasStringPrefix(_prefix, ref argPos))
             {
                 if (msg.Author.IsBot) return;
+                if (msg.Content == _prefix) return;
 
-                IResult result = await _service.ExecuteAsync(context, argPos);
+                IResult result = await _service.ExecuteAsync(context, argPos);             
+
                 if (!result.IsSuccess)
                 {
                     if (result.Error != CommandError.UnknownCommand)
@@ -45,21 +51,12 @@ namespace EdgyCore
                         }
                         else
                         {
-                            await context.Channel.SendMessageAsync($"You gave wrong input in this command. Check the **{_lib.GetPrefix()}help** command for info.");
+                            await context.Channel.SendMessageAsync($"You gave wrong input in this command. Check the **{_prefix}help** command for info.");
                         }
                     }
                     else
                     {
-                        if (msg.Content == "e!") return;
                         await context.Message.AddReactionAsync(new Emoji("❓"));
-                        //await context.Message.AddReactionAsync(new Emoji("❌"));
-                    }
-                }
-                else
-                {
-                    if (!context.User.IsBot)
-                    {
-                        //_database.AddExperience(context.User.Id);
                     }
                 }
             }
