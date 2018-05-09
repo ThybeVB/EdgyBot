2a6352d0394e26c6fc818ec6028c65d7d8451691
MonstahGames <monstahhhgames@gmail.com>
2018-05-09 21:33:27 +0200

    Image Command Test Complete

--- EdgyBot/EdgyBot/Modules/Categories/ImageCommands.cs
+++ EdgyBot/EdgyBot/Modules/Categories/ImageCommands.cs
@@ -13,6 +13,7 @@ namespace EdgyCore.Modules.Categories
     public class ImageCommands : ModuleBase<SocketCommandContext>
     {
         private readonly LibEdgyBot _lib = new LibEdgyBot();
+        WebClient client = new WebClient();
 
         [Command("imagecommands"), Alias("image", "images", "imgcommands"), Name("imagecommands"), Summary("Some info on why there currently can be no Image Commands")]
         public async Task ImageCommandsInfo ()
@@ -21,6 +22,7 @@ namespace EdgyCore.Modules.Categories
             eb.AddField("Why no Image Commands?", "When an Image Command is executed, The image first gets downloaded to the Machine. There are some problems with this.");
             eb.AddField("Problems?", "The Hosting Service that EdgyBot uses does not allow downloading external files from code. This is why EdgyBot has no Image Commands");
             eb.AddField("Fix?", "Getting a Virtual Private Server (VPS) will resolve this issue, But until that, I can't provide Images.");
+            eb.AddField("Edit", "I finally got myself a VPS, working on some image commands :smile:");
 
             await ReplyAsync("", embed: eb.Build());
         }
@@ -28,16 +30,18 @@ namespace EdgyCore.Modules.Categories
         [Command("imgtest", RunMode = RunMode.Async)][RequireOwner]
         public async Task ImgTestCmd ()
         {
-            Attachment img = Context.Message.Attachments.SingleOrDefault();
-
+            Attachment img = Context.Message.Attachments.FirstOrDefault();
             if (img == null)
             {
-                await ReplyAsync("Please provide an image.");
-                return;
+                string avUrl = Context.Message.Author.GetAvatarUrl();
+                await client.DownloadFileTaskAsync(new Uri(avUrl), "C:/EdgyBot/DownloadedImages/imgtest.png");
+                await Context.Channel.SendFileAsync("C:/EdgyBot/DownloadedImages/imgtest.png");
+            } else
+            {
+                await client.DownloadFileTaskAsync(new Uri(img.Url), "C:/EdgyBot/DownloadedImages/imgtest.png");
+                await Context.Channel.SendFileAsync("C:/EdgyBot/DownloadedImages/imgtest.png");
             }
-            WebClient client = new WebClient();
-            await client.DownloadFileTaskAsync(new Uri(img.Url), "UserImg/imgTest.png");
-            await Context.Channel.SendFileAsync("UserImg/imgTest.png");
+            File.Delete("C:/EdgyBot/DownloadedImages/imgtest.png");
         }
     }
 }
