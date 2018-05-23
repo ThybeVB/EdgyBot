625f7e7ac1ae0981e929e1b4e8ae34b4d6e81765
MonstahGames <30873388+MonstahGames@users.noreply.github.com>
2018-05-23 16:55:53 +0200

    more

--- EdgyBot/EdgyBot/Handler/Pinger/BotListAPI/DiscordBotsPinger.cs
+++ EdgyBot/EdgyBot/Handler/Pinger/BotListAPI/DiscordBotsPinger.cs
@@ -7,11 +7,18 @@ namespace EdgyCore.Handler.Pinger
 {
     public class DiscordBotsPinger
     {
-        private JsonHelper helper = new JsonHelper("");
+        private JsonHelper helper = new JsonHelper("https://www.bots.discord.pw/api/bots/" + EdgyBot.Credientals.clientID + "/stats");
+        private LibEdgyBot _lib = new LibEdgyBot();
 
         public async Task PostServerCountAsync ()
         {
+            var dict = new Dictionary<string, object>();
+            dict.Add("server_count", _lib.GetServerCount());
 
+            try { helper.postDataDiscordBots(dict); } catch (Exception e)
+            {
+                await _lib.EdgyLog(Discord.LogSeverity.Error, "Failed to post data to Bots For Discord:\n" + e.Message);
+            }
         }
     }
 }
