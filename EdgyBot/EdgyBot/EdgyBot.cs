0979b4bc36f2af869939a83c519e864440695c86
MonstahGames <monstahhhgames@gmail.com>
2018-06-02 16:09:48 +0200

    DSC

--- EdgyBot/EdgyBot/EdgyBot.cs
+++ EdgyBot/EdgyBot/EdgyBot.cs
@@ -12,16 +12,20 @@ namespace EdgyCore
 
         public static Credentials Credientals;
 
-        public readonly DiscordSocketClient Client = new DiscordSocketClient(new DiscordSocketConfig { LogLevel = LogSeverity.Verbose });
+        public readonly DiscordShardedClient Client = new DiscordShardedClient(new DiscordSocketConfig
+        {
+            LogLevel = LogSeverity.Verbose
+        });
+        //public readonly DiscordSocketClient Client = new DiscordSocketClient(new DiscordSocketConfig { LogLevel = LogSeverity.Verbose });
         private readonly LibEdgyBot _lib = new LibEdgyBot();
 
         private async Task StartAsync ()
         {
             Credientals = _lib.GetCredientals();
-            new EventHandler(Client);
+            //new EventHandler(Client);
             await Client.LoginAsync(TokenType.Bot, Credientals.token);
             await Client.StartAsync();
-            await new CommandHandler().InitializeAsync(Client);
+            //await new CommandHandler().InitializeAsync(Client);
 
             await Task.Delay(-1);
         }
