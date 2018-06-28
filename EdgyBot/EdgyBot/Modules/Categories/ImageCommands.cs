b0dc4fd28a4f75d547ba0c32b02b4c01fd27c8b9
MonstahGames <monstahhhgames@gmail.com>
2018-06-28 17:21:00 +0200

    Deepfry Command

--- EdgyBot/EdgyBot/Modules/Categories/ImageCommands.cs
+++ EdgyBot/EdgyBot/Modules/Categories/ImageCommands.cs
@@ -16,7 +16,7 @@ namespace EdgyBot.Modules.Categories
         private readonly LibEdgyBot _lib = new LibEdgyBot();
         private ImgLib imgLib = new ImgLib();
 
-        [Name("pixelate"), Summary("Pixelizes an Image")]
+        [Name("pixelate"), Summary("Pixelizes an image")]
         [Command("pixelate", RunMode = RunMode.Async), Alias("pixelize", "pixel")]
         public async Task PixelCmd (IUser usr = null)
         {
@@ -36,11 +36,35 @@ namespace EdgyBot.Modules.Categories
             File.Delete(filePath + fileName);
         }
 
-        [Command("greyscale", RunMode = RunMode.Async), Alias("grey", "grayscale")]
-        [Name("greyscale"), Summary("Greyscales an image.")]
+        [Command("deepfry", RunMode = RunMode.Async), Alias("df")]
+        [Name("deepfry"), Summary("Deepfries an image")]
+        public async Task DeepfryCmd (IUser usr = null)
+        {
+            string fileName = "df.png";
+
+            Attachment image = Context.Message.Attachments.FirstOrDefault();
+            await imgLib.DetermineAttachmentAndDownload(image, Context, fileName, usr);
+
+            Image<Rgba32> img = imgLib.OpenImage(fileName);
+            img.Contrast(99);
+            img.Save(filePath + fileName);
+            img.Dispose();
+
+            Image<Rgba32> img2 = imgLib.OpenImage(fileName);
+            img2.Contrast(30);
+            img2.Save(filePath + fileName);
+
+            await Context.Channel.SendFileAsync(filePath + fileName);
+
+            img2.Dispose();
+            File.Delete(filePath + fileName);
+        }
+
+        [Command("grayscale", RunMode = RunMode.Async), Alias("grey", "greyscale")]
+        [Name("grayscale"), Summary("Grayscales an image (black and white)")]
         public async Task GreyscaleCmd (IUser usr = null)
         {
-            string fileName = "yo.png";
+            string fileName = "grayscale.png";
 
             Attachment image = Context.Message.Attachments.FirstOrDefault();
             await imgLib.DetermineAttachmentAndDownload(image, Context, fileName, usr);
