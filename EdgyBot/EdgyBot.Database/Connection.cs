8e60e30e7cf4696cc81973e2ca40b297b13579e5
MonstahGames <monstahhhgames@gmail.com>
2018-07-01 14:12:31 +0200

    Database Base

---
+++ EdgyBot/EdgyBot.Database/Connection.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace EdgyBot.Database
+{
+    public class Connection
+    {
+        public SqliteConnection connectionObject { get; set; }
+        public bool isOpened { get; set; }
+    }
+}
---
+++ EdgyBot/EdgyBot.Database/DatabaseConnection.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace EdgyBot.Database
+{
+    public class DatabaseConnection
+    {
+        private SqliteConnection _connObj;
+
+        string _dbFileName = "";
+        string _fullDir = "C:/EdgyBot/Database/";
+
+        public DatabaseConnection (string dbFileName, string fullDir = "")
+        {
+            _dbFileName = dbFileName;
+
+            if (!string.IsNullOrEmpty(fullDir))
+                _fullDir = fullDir;
+            
+        }
+
+        public Task ConnectAsync ()
+        {
+            SqliteConnection connection = new SqliteConnection("" +
+            new SqliteConnectionStringBuilder
+            {
+                DataSource = _dbFileName
+            });
+
+            if (connection != null)
+                _connObj = connection;
+
+            return Task.CompletedTask;
+        }
+
+        public async Task<Connection> OpenConnection ()
+        {
+            try
+            {
+                await _connObj.OpenAsync();
+
+                Connection connection = new Connection
+                {
+                    connectionObject = _connObj,
+                    isOpened = true
+                };
+                return connection;
+
+            } catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+    }
+}
