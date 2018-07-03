d745e6accf0b6282818683c3a2adbbd2a8a60ae5
MonstahGames <monstahhhgames@gmail.com>
2018-07-03 15:10:17 +0200

    remùoved a thing

--- EdgyBot/EdgyBot/Lib/ImgLib.cs
+++ EdgyBot/EdgyBot/Lib/ImgLib.cs
@@ -12,35 +12,14 @@ namespace EdgyCore
         private WebClient webClient = new WebClient();
         private readonly string fileLocation = "C:/EdgyBot/DownloadedImages/";
 
-        /// <summary>
-        /// Downloads an Image.
-        /// </summary>
-        /// <param name="url"></param>
-        /// <param name="fileName"></param>
-        /// <returns></returns>
         public async Task DownloadAsync(Uri url, string fileName)
             => await webClient.DownloadFileTaskAsync(url, fileLocation + fileName);
-        
 
-        /// <summary>
-        /// Properly opens a ImageSharp image (RGBA32)
-        /// </summary>
-        /// <param name="fileName"></param>
-        /// <returns></returns>
         public Image<Rgba32> OpenImage(string fileName)
         {
             return ImageSharp.Image.Load(fileLocation + fileName);
         }
 
-
-        /// <summary>
-        /// Determines if the Attachment is a Profile Picture or an Attachment.
-        /// Returns the Attachment after the check.
-        /// </summary>
-        /// <param name="img"></param>
-        /// <param name="Context"></param>
-        /// <param name="fileName"></param>
-        /// <returns></returns>
         public async Task DetermineAttachmentAndDownload(Attachment img, SocketCommandContext Context, string fileName, IUser usr = null)
         {
             if (usr != null)
