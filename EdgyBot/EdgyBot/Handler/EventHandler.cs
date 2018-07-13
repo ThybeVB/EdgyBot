9df2ac1025eb2e15c98d7979a1a7a7fd5bc86293
Thybe Van Beersel <vanbeerselthybe@gmail.com>
2018-07-13 18:19:57 +0200

    Missing a line

--- EdgyBot/EdgyBot/Handler/EventHandler.cs
+++ EdgyBot/EdgyBot/Handler/EventHandler.cs
@@ -11,30 +11,30 @@ namespace EdgyCore.Handler
     {
         private DiscordShardedClient _client;
 
+        private int _shardsConnected = 0;
+
+        private LibEdgyCore _coreLib = new LibEdgyCore();
+        private LibEdgyBot _lib = new LibEdgyBot();
+
         private DiscordBotsPinger _dbPinger = new DiscordBotsPinger();
         private BotsForDiscordPinger _bfdPinger = new BotsForDiscordPinger();
         private static DBLPinger dblPinger = new DBLPinger();
         private BotListSpacePinger blspPinger = new BotListSpacePinger();
         private ListcordPinger listcordPinger = new ListcordPinger();
 
-        private LibEdgyCore _coreLib = new LibEdgyCore();
-        private LibEdgyBot _lib = new LibEdgyBot();
         public static int MemberCount;
         public static int ServerCount;
-
-        private int _shardsConnected = 0;
-        
         public static SocketUser OwnerUser;
 
-        public EventHandler(DiscordShardedClient client)
+        public EventHandler (DiscordShardedClient client)
         {
             _client = client;
 
             _client.Log += _lib.Log;
             _client.ShardReady += ShardReady;
+            _client.ShardDisconnected += ShardDisconnected;
             _client.JoinedGuild += JoinedGuild;
             _client.LeftGuild += LeftGuild;
-            _client.ShardDisconnected += ShardDisconnected;
             _client.UserJoined += UserUpdated;
             _client.UserLeft += UserUpdated;
         }
@@ -50,7 +50,7 @@ namespace EdgyCore.Handler
             }  
         }
 
-        public async Task SetupBot()
+        public async Task SetupBot ()
         {
             if (OwnerUser == null)
                 OwnerUser = _client.GetUser(_coreLib.GetOwnerID());
@@ -61,13 +61,13 @@ namespace EdgyCore.Handler
             await RefreshBotAsync(true);
         }
 
-        private Task UserUpdated(SocketGuildUser arg)
+        private Task UserUpdated (SocketGuildUser arg)
         {
             MemberCount = CalculateMemberCount();
             return Task.CompletedTask;
         }
 
-        private async Task ShardDisconnected(Exception exception, DiscordSocketClient client)
+        private async Task ShardDisconnected (Exception exception, DiscordSocketClient client)
            => await _lib.EdgyLog(LogSeverity.Critical, $"EDGYBOT SHARD {client.ShardId} HAS SHUT DOWN WITH AN EXCEPTION, \n{exception.Source}: {exception.Message}\n{exception.StackTrace}");
 
         private async Task JoinedGuild(SocketGuild guild)
@@ -79,13 +79,15 @@ namespace EdgyCore.Handler
             await RefreshBotAsync();
         }
 
-        private async Task LeftGuild(SocketGuild guild)
+        private async Task LeftGuild (SocketGuild guild)
         {
             ServerCount = _client.Guilds.Count;
+            MemberCount = CalculateMemberCount();
+
             await RefreshBotAsync();
         }
 
-        private int CalculateMemberCount()
+        private int CalculateMemberCount ()
         {
             int users = 0;
             foreach (SocketGuild guild in _client.Guilds)
@@ -98,7 +100,7 @@ namespace EdgyCore.Handler
             return users;
         }
 
-        private async Task RefreshBotAsync(bool startup = false)
+        private async Task RefreshBotAsync (bool startup = false)
         {
             string gameStatus = "e!help | EdgyBot for " + ServerCount + " servers!";
             await _client.SetGameAsync(gameStatus);
