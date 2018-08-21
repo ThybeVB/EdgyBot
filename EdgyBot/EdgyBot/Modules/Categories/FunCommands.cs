539631a65031ee18fb2e1088eda6b3e7a6e80006
MonstahGames <30873388+MonstahGames@users.noreply.github.com>
2018-08-21 17:46:56 +0200

    Published 8ball command

--- EdgyBot/EdgyBot/Modules/Categories/FunCommands.cs
+++ EdgyBot/EdgyBot/Modules/Categories/FunCommands.cs
@@ -14,18 +14,19 @@ namespace EdgyBot.Modules.Categories
     public class FunCommands : ModuleBase<ShardedCommandContext>
     {
         private readonly LibEdgyBot _lib = new LibEdgyBot();
-        private string[] ballReplies = { "It is certain.", "Without a doubt.", "You may rely on it.", 
-                                "As I see it, yes.", "Yes.", "Better not tell you now.", "Concentrate and ask again.",
-                                "My reply is no.", "My sources say no", "Very doubtful." };
+        private string[] ballReplies = {"It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", 
+            "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", 
+            "Reply hazy, try again", "Ask again later.", "Cannot predict now.", "Concentrate and ask again.", 
+            "Don't count on it.", "My reply is no.", "My sources say no", "Outlook not so good.", "Very doubtful"};
 
         [Command("8ball")]
+        [Name("8ball"), Summary("Ask a question to the 8ball")]
         public async Task EightBallCmd ([Remainder]string question) 
         {
             Random random = new Random();
             int index = random.Next(ballReplies.Count());
 
-            string responseString = $":8ball: {Context.User.Mention}: {question}\nMy Response: {ballReplies[index]}";
-
+            string responseString = $"{Context.User.Mention}: {question}\n:8ball:: {ballReplies[index]}";
             await ReplyAsync(responseString);
         }
 
